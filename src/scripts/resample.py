8e115258014daf92bd7fe3265f42f0cc106e8147
Vladyslav Zalevskyi <vladyslav.zalevskyi@unil.ch>
2024-07-23 09:52:56 +0200

    add session support

--- src/scripts/resample.py
+++ src/scripts/resample.py
@@ -9,7 +9,7 @@ import monai
 res = 0.5
 
 # INPUT PATH (BIDS)
-bids_path = Path('/bids/path')
+bids_path = Path('/media/vzalevskyi/data/fetal_dHCP/derivatives')
 
 # INPUT TARGET SIZE
 target_size = (256, 256, 256)
@@ -42,27 +42,29 @@ subjects = list(bids_path.glob('sub-*'))
 print(f'Found {len(subjects)} in {bids_path}')
 
 for sub in tqdm(subjects):
-    saver = monai.transforms.SaveImaged(keys=['image', 'label'],
-                                        output_dir=out_path/f'{sub.name}/anat/',
-                                        output_postfix='',
-                                        resample=False,
-                                        separate_folder=False,
-                                        print_log=False,
-                                        allow_missing_keys=True,
-                                        mode='nearest')
+    for ses in sub.glob('ses-*'):
+        sesion = ses.name
+        saver = monai.transforms.SaveImaged(keys=['image', 'label'],
+                                            output_dir=out_path/f'{sub.name}/{sesion}/anat/',
+                                            output_postfix='',
+                                            resample=False,
+                                            separate_folder=False,
+                                            print_log=False,
+                                            allow_missing_keys=True,
+                                            mode='nearest')
 
-    try:
-        imgs = list(sub.glob('**/*_T2w.nii.gz'))[0]  # CHANGE THE PATTERN TO MATCH YOUR IMAGES
-        label = list(sub.glob('**/*_dseg.nii.gz'))[0]  # CHANGE THE PATTERN TO MATCH YOUR LABELS
-        data = loader({'image': str(imgs), 'label': str(label)})
-        data['image'] = data['image'].unsqueeze(0)
-        data['label'] = data['label'].unsqueeze(0)
-        data['label'] = data['label'].squeeze(-1)
-        data = resampe_transform(data)
-        data = orientation(data)
-        data = cropper(data)
-        data = padder(data)
-        saver(data)
-    except Exception as e:
-        print(f'Error processing {sub} due to {e}')
-        continue
+        try:
+            imgs = list(sub.glob(f'{sesion}/anat/*_T2w.nii.gz'))[0]  # CHANGE THE PATTERN TO MATCH YOUR IMAGES
+            label = list(sub.glob(f'{sesion}/anat/*drawem9_dseg.nii.gz'))[0]  # CHANGE THE PATTERN TO MATCH YOUR LABELS
+            data = loader({'image': str(imgs), 'label': str(label)})
+            data['image'] = data['image'].unsqueeze(0)
+            data['label'] = data['label'].unsqueeze(0)
+            data['label'] = data['label'].squeeze(-1)
+            data = resampe_transform(data)
+            data = orientation(data)
+            data = cropper(data)
+            data = padder(data)
+            saver(data)
+        except Exception as e:
+            print(f'Error processing {sub} due to {e}')
+            continue
